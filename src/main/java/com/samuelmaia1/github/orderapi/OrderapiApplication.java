1edfc964a6eef46d27d21fad01b9d473597a91ed
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2025-05-26 21:05:57 -0300

    Criando projeto e configurando conexão com banco de dados

---
+++ src/main/java/com/samuelmaia1/github/orderapi/OrderapiApplication.java
@@ -0,0 +1,13 @@
+package com.samuelmaia1.github.orderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuelmaia1/github/orderapi/OrderapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelmaia1.github.orderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
